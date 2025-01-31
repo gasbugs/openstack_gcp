e26ddda398046a0ed77f0c082f92ff1cf7330a36
Ilsun Choi <ilsunchoi@cloudsecuritylab.co.kr>
2025-01-31 20:04:12 +0900

    NAT 게이트웨이 추가

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  project = "sds-openstack-03" # GCP 프로젝트 ID로 변경하세요
-  region  = "us-west1"         # 원하는 리전으로 변경하세요
+  project = "cloudslab003-tranmoon69" # GCP 프로젝트 ID로 변경하세요
+  region  = "us-west1"                # 원하는 리전으로 변경하세요
   zone    = var.zone
 }
 
@@ -20,6 +20,48 @@ resource "google_compute_network" "custom_networks" {
 resource "random_integer" "id" {
   min = 1
   max = 50000
+}
+
+# Cloud Router 생성
+resource "google_compute_router" "router" {
+  name    = "nat-router"
+  network = google_compute_network.custom_networks[0].name
+}
+
+# Cloud NAT 게이트웨이 생성
+resource "google_compute_router_nat" "nat" {
+  name                               = "nat-gateway"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
+
+# 필요한 방화벽 규칙 설정 (예시)
+resource "google_compute_firewall" "allow_internal" {
+  name    = "allow-internal"
+  network = google_compute_network.custom_networks[0].name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["10.4.20.0/24"]
 }
 
 resource "google_compute_subnetwork" "custom_subnets" {
